ff4465b839994aa2e3e59761882b5d1bf925a509
JoseOgam <jotienoochieng@gmail.com>
2020-10-13 13:40:15 -0400

    webpackConfig api

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require("path")
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader",
+                } 
+            },
+        ],
+    },
+}
